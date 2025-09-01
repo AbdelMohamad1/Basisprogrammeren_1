11ace76e03c0e867ce8faad6e74a949b9277b3ab
Abdelrahman Mohamad (student) <s1227562@student.windesheim.nl>
2025-09-01 09:56:42 +0200

    add Leeftijd in maanden

---
+++ Week1Opdracht2/main.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+
+//Declareren van integers a,b,enc
+int a, b, c;
+
+
+int main() {
+
+
+	scanf_s("%d", &a); 
+		scanf_s("%d", &b);
+
+	c = a + b;
+
+
+
+	printf_s("Het antwoord van %d/%d is: %d.",a,b,c);
+
+}
---
+++ Week1Opdracht_i1/main.cpp
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+
+int a, b, c;
+
+int main() {
+	b = 12;
+	printf_s("Hoe oud bent u ");
+		scanf_s("%d", &a);
+
+		c = a * b;
+
+	printf_s("Uw leeftijd in maanden is: %d",c);
+}
